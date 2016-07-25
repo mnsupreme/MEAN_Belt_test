2b7894172a4905c1f87e66b48ee6b2f6b74773f9
Mitchell Lee <mitchell@Users-MacBook.charter.com>
2016-07-24 23:12:30 -0700

    set up

---
+++ client/assets/controllers/controller.js
@@ -0,0 +1,6 @@
+app.controller('FactoryNames(plural-capital naming convention)', ["$scope", 'friendFactory', '$routeParams', '$location', function($scope, friendFactory, $routeParams, $location){
+		
+
+		
+
+	}])
---
+++ client/assets/factories/factory.js
@@ -0,0 +1,7 @@
+app.factory('defaultFactory (camel case singular)', ['$http', function($http){
+  console.log("Factory loaded!");
+  var factory={};
+  
+  return factory
+}])
+
