d8e6a5bb21c88b64cd5c2b27bfc40ec8fcd7c6d5
Mitchell Lee <mitchell@Users-MacBook.local>
2016-07-29 10:20:53 -0700

    s

--- server/models/User.js
+++
@@ -1,8 +0,0 @@
-console.log('user model');
-var mongoose = require('mongoose');
-// build your friend schema and add it to the mongoose.models
-var UserSchema = new mongoose.Schema({
-  name: String,
-}, {timestamps: true });
-// register the schema as a model
-var User = mongoose.model('User', UserSchema);
